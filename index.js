9ee4c9003932781390ffcca6478c0146258ec8d5
Lars <larsbrogaard86@gmail.com>
2022-03-21 20:23:58 +0100

    Library template

--- index.js
+++ index.js
@@ -14,3 +14,28 @@ function renderNavItems(evt){
 
 document.getElementById("navbar").onclick= renderNavItems;
 showPage("front-page")
+
+
+// Library page
+//document.getElementById("btn-get-library").onclick = getLibrary;
+
+document.getElementById("btn-get-all-libraries").onclick = getAllLibraries;
+
+function getAllLibraries(){
+    fetch("http://localhost:8080/api/libraries")
+    .then(res => res.json())
+    .then(data => {
+        console-log(data)
+        const rows = data.map(u=>
+        `
+        <tr>
+            <td>${u.id}</td>
+            <td>${u.name}
+        </tr>    
+        `).join("\n")
+        document.getElementById("tbl-id").innerHTML=rows;
+    })
+
+    .catch(err => console.log("OOOPPs: " + err))
+    .finally(err => console.log("Done"))
+}
--- js-for-pages/library.js
+++ js-for-pages/library.js
@@ -1,9 +1,23 @@
 // https://localhost:8080/api/libraries
 
-document.getElementById("btn-get").onclick = getLibrary;
-document.getElementById("btn-get-all").onclick = getAllLibraries;
+document.getElementById("btn-get-library").onclick = getLibrary;
+document.getElementById("btn-get-all-libraries").onclick = getAllLibraries;
 
 function getAllLibraries(){
     fetch("http://localhost:8080/api/libraries")
-    .then(res => res.json)
+    .then(res => res.json())
+    .then(data => {
+        console-log(data)
+        const rows = data.map(u=>
+        `
+        <tr>
+            <td>${u.id}</td>
+            <td>${u.name}
+        </tr>    
+        `).join("\n")
+        document.getElementById("tbl-id").innerHTML=rows;
+    })
+
+    .catch(err => console.log("OOOPPs: " + err))
+    .finally(err => console.log("Done"))
 }
